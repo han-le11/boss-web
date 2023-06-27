639c286ec9c3b8cd5d63a6b1ff98019ca10f817b
han.le@aalto.fi <le.nm.han@gmail.com>
2023-06-28 00:11:52 +0300

    combine tab Input data to tab Run BOSS. update with params service

--- src/app.py
+++ src/app.py
@@ -1,45 +1,28 @@
 import streamlit as st
 from boss.bo.bo_main import BOMain
-from page_config import Config
-from file_reader import FileUploader
+from params_service import ParamsService
+from page_config import PageConfig
+from file_handler import upload_file, choose_inputs_and_outputs
 
-Config.set_page()
-Config.set_main_title()
+page_config = PageConfig
+page_config.set_page()
+page_config.set_main_title()
+params = ParamsService
 
 
 def dummy_function(_):
     pass
 
 
-home, input_data, run, postprocessing, tutorial = st.tabs(["🏠 Home",
-                                                           "Input data",
-                                                           "Run BOSS",
-                                                           "Post-processing",
-                                                           "Tutorial"])
-with input_data:
-    uploaded_file = FileUploader.upload_file()
-    X, Y, X_names, Y_name = FileUploader.choose_inputs_and_outputs(uploaded_file)
-
+home, run, postprocessing, tutorial = st.tabs(["🏠 Home",
+                                               "Run BOSS",
+                                               "Post-processing",
+                                               "Tutorial"])
 with run:
+    uploaded_file = upload_file()
+    X, Y, X_names, Y_name = choose_inputs_and_outputs(uploaded_file)
+    bounds = params.input_x_bounds(X_names)
     noise = st.number_input('Data noise', min_value=0.0)
-
-    lower1, upper1 = st.columns(2)
-    with lower1:
-        lower_bound_1 = st.number_input('Lower bound of input variable 1')
-    with upper1:
-        upper_bound_1 = st.number_input('Upper bound of input variable 1')
-
-    lower2, upper2 = st.columns(2)
-    with lower2:
-        lower_bound_2 = st.number_input('Lower bound of input variable 2')
-    with upper2:
-        upper_bound_2 = st.number_input('Upper bound of input variable 2')
-
-    y_range_lower, y_range_upper = st.columns(2)
-    with y_range_lower:
-        y_range_lower_bound = st.number_input('Estimated lower value of output')
-    with y_range_upper:
-        y_range_upper_bound = st.number_input('Estimated upper value of output')
 
     initpts, iterpts = st.columns(2)
     with initpts:
@@ -47,10 +30,12 @@ with run:
     with iterpts:
         iters = st.number_input('Number of iterations', min_value=0, step=1)
 
+    y_range_lower_bound, y_range_upper_bound = params.input_y_range()
+
     if st.button("Run BOSS"):
         bo = BOMain(
             dummy_function,
-            bounds=[[lower_bound_1, upper_bound_1], [lower_bound_2, upper_bound_2]],
+            bounds=bounds,
             yrange=[y_range_lower_bound, y_range_upper_bound],
             kernel='rbf',
             noise=noise,
@@ -67,4 +52,3 @@ with run:
     # if st.button('Next acquisition'):
     #     X_next = res.get_next_acq(-1)
     #     st.write(X_next)
-
