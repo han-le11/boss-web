f8a952752ae9c87ceb8aa73245ff74f34306a586
han.le@aalto.fi <le.nm.han@gmail.com>
2023-07-04 18:32:16 +0300

    made a home page and a tutorial page

---
+++ src/home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="Home - Bayesian Optimization Structure Search",
+                   page_icon="🏠",
+                   initial_sidebar_state="expanded",
+                   menu_items={
+                       'Get Help': 'https://www.boss.io/help'
+                        }
+                   )
+
+st.markdown("# Bayesian Optimization Structure Search (BOSS)")
