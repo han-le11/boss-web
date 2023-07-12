6301b8824fe5143381c8da4f677f06d0fe90aef1
han.le@aalto.fi <le.nm.han@gmail.com>
2023-07-12 17:04:27 +0300

    remove plus/minus toggles in input fields

--- src/services/run_page_config.py
+++ src/services/run_page_config.py
@@ -5,14 +5,20 @@ class RunPageConfig:
     @staticmethod
     def set_page():
         st.set_page_config(
-            page_title="Bayesian Optimization Structure Search (BOSS)",
-            layout="centered",
-            initial_sidebar_state="expanded",
-            menu_items={
-                'Get Help': 'https://www.boss.ai/help'
-            }
+            page_title="Run BOSS", layout="centered", initial_sidebar_state="expanded"
         )
 
     @staticmethod
     def set_main_title():
-        st.title('Bayesian Optimization Structure Search (BOSS)')
+        st.title("🏃‍♀️ Run BOSS")
+
+    @staticmethod
+    def hide_toggles():
+        st.markdown("""
+                    <style>
+                        button.step-up {display: none;}
+                        button.step-down {display: none;}
+                        div[data-baseweb] {border-radius: 4px;}
+                    </style>""",
+                    unsafe_allow_html=True
+                    )
