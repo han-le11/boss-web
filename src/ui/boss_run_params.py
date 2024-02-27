17867ec98a84813c0e5f3bc76c0c6e14bc264ec1
han.le@aalto.fi <le.nm.han@gmail.com>
2024-02-27 15:23:28 +0200

    Add type hints and removed unnecessary function set_y_range()

--- src/ui/boss_run_params.py
+++ src/ui/boss_run_params.py
@@ -9,7 +9,7 @@ def _check_bounds(lower_bound, upper_bound):
         st.error("⚠️ Warning: lower bound has to be smaller than upper bound.")
 
 
-def _return_bounds(X_names, d) -> (float, float):
+def _return_bounds(X_names: list[str], d: int) -> (float, float):
     left_col, right_col = st.columns(2)
     with left_col:
         lower_bound = st.number_input(
@@ -26,11 +26,10 @@ def _return_bounds(X_names, d) -> (float, float):
     return lower_bound, upper_bound
 
 
-def input_x_bounds(X_names) -> np.ndarray:
+def input_x_bounds(X_names: list[str]) -> np.ndarray:
     """
     Display the number input widgets based on the dimension and input variable names.
-    :param X_names: list(str)
-        Input variable names
+    :param X_names: Input variable names
     """
     dimension = len(X_names)
     bounds = np.empty(shape=(dimension, 2))
@@ -43,46 +42,28 @@ def input_x_bounds(X_names) -> np.ndarray:
     return bounds
 
 
-def set_y_range(y_values, y_name) -> (float, float):
-    """
-    Find and display min value and max value of the target variable.
-    These values are given to BOSS.
-    """
-    y_min, y_max = None, None
-    if y_name and y_values.size != 0 or not np.isnan(y_values).any:
-        y_min = np.amin(y_values)
-        y_max = np.amax(y_values)
-    # else:
-    #     st.error("Please check that target values are filled.")
-    return y_min, y_max
-
-
 # TODO
 def set_optional_params():
     """
-    Set advanced, optional parameters
+    Set advanced, optional parameters.
     :return:
     """
     pass
 
 
-def dummy_function(_):
-    pass
-
-
-def parse_data_and_bounds(df, dim):
+def parse_data_and_bounds(df: pd.DataFrame, dim: int):
     """
     If there's a dataframe of initial points in the session state, then parse input variables as X, target variables
     as Y.
-    :param df:
-    :param dim:
+    :param df: dataframe of initial data points
+    :param dim: dimension
     :return:
     """
     n_columns = df.shape[1]
     X_vals = df.iloc[:, 0:dim].to_numpy()
     Y_vals = df.iloc[:, dim].to_numpy()
-    X_names = list(df.columns.values)[0:dim]
-    y_name = list(df.columns.values)[dim]
+    X_names = df.columns.to_list()[0:dim]
+    y_name = df.columns.to_list()[dim]
 
     inputs, outputs = st.columns(2)
     with inputs:
@@ -90,9 +71,8 @@ def parse_data_and_bounds(df, dim):
     with outputs:
         st.write("Target variable", Y_vals)
 
-    y_min, y_max = set_y_range(Y_vals, y_name)
     X_bounds = df.iloc[0:2, dim + 1:n_columns].to_numpy()
     st.write("Bounds", X_bounds)
     X_bounds = np.transpose(X_bounds)
 
-    return X_vals, X_names, Y_vals, X_bounds, y_min, y_max
+    return X_vals, X_names, Y_vals, X_bounds
