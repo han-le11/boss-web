e375f5beecef2c34336763e22cab3fbb784cb2c6
han.le@aalto.fi <le.nm.han@gmail.com>
2024-02-27 16:16:06 +0200

    Functions to extract bounds of input variables and column data from a file.

--- src/ui/file_handler.py
+++ src/ui/file_handler.py
@@ -1,12 +1,12 @@
-import logging
-import streamlit as st
+import numpy as np
 import pandas as pd
-from dateutil.parser import ParserError
+import streamlit as st
+from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 
 def upload_file():
     """
-    Widget to upload a file.
+    Widget to upload a file, which is read into a dataframe.
     Display the "help" tip when hovering the mouse over the question mark icon.
     """
     file = st.file_uploader(
@@ -14,7 +14,56 @@ def upload_file():
         type=["csv"],
         help="Your file should contain data for input variables and target variable",
     )
-    return file
+    if file is not None and isinstance(file, UploadedFile):
+        try:
+            df = pd.read_csv(file, sep=":|;|,")
+            return df
+        except ValueError as err:
+            st.error("Error: " + str(err) + ". Please check the file contents and re-upload.")
+    else:
+        raise ValueError("Please provide a single csv file.")
+
+
+def check_if_there_are_bounds(df) -> bool:
+    """
+    Returns True if there are bounds in the uploaded file; otherwise False.
+    :param df: data from the uploaded file that is read into a dataframe.
+    :return:
+    """
+    bounds_exist = False
+    if df is not None:
+        x = df.filter(regex='boss-bound').dropna().to_numpy()
+        if x.size != 0:
+            # get a list of bound names
+            bound_names = [k for k in list(df.columns) if 'boss-bound' in k]
+            if len(bound_names) != 0:
+                #  and len(bound_names) == len(X_names)
+                bounds_exist = True
+    return bounds_exist
+
+
+def parse_bounds(df):
+    """
+    Return the variable names and bounds for BOMain object.
+    :param df: Uploaded file, which is read into a dataframe.
+    :return:
+    """
+    if df is not None:
+        bounds_array = df.filter(regex="boss-bound").dropna().to_numpy()
+        transpose_bounds_array = np.transpose(bounds_array)
+        st.write("Found bounds: ", transpose_bounds_array)
+        return transpose_bounds_array
+
+
+def extract_col_data(df: pd.DataFrame, keyword: str) -> np.array:
+    """
+    Get the column(s) whose name contains the given keyword.
+    :param df:
+    :param keyword:
+    :return:
+    """
+    array = df.filter(regex=keyword).to_numpy()
+    return array
 
 
 # TODO
@@ -22,49 +71,42 @@ def display_placeholder_text_in_multiselect_box():
     pass
 
 
-def choose_inputs_and_outputs(uploaded_file):
+def choose_inputs_and_outputs(df):
     """
-    Let users choose at least one column for input variable(s) and only one column for target variable.
-    :param uploaded_file:
-        A file (either csv or excel) uploaded by a user
+    Display widgets that let users choose at least one column for input variable(s) and
+    only one column for target variable.
+    :param df:
+        Dataframe read from the UploadedFile object (file uploaded by a user).
     """
-    input_names = []
-    output_name = []
-    x = []
-    y = []
+    X_names = []
+    Y_name = []
+    X_vals = []
+    Y_vals = []
 
-    if uploaded_file is not None:
-        df = None
-        try:
-            df = pd.read_csv(uploaded_file, sep=":|;|,")
-        except ValueError as err:
-            st.error("Error: " + str(err) + " Please check and upload the file again.")
+    if df is not None:
+        in_col, out_col = st.columns(2)
+        with in_col:
+            X_names = st.multiselect(
+                "Choose input variables *", options=list(df.columns), default=None
+            )
+        with out_col:
+            Y_name = st.multiselect(
+                "Choose one target variable *",
+                options=list(df.columns),
+                default=None,
+                max_selections=1,
+            )
+        X_vals = df[X_names]
+        Y_vals = df[Y_name]
 
-        if df is not None:  # successful
-            in_col, out_col = st.columns(2)
-            with in_col:
-                input_names = st.multiselect(
-                    "Choose input variables *", options=list(df.columns), default=None
-                )
-            with out_col:
-                output_name = st.multiselect(
-                    "Choose one target variable *",
-                    options=list(df.columns),
-                    default=None,
-                    max_selections=1,
-                )
-            x = df[input_names]
-            y = df[output_name]
-
-            with st.expander("See chosen data"):
-                ins, outs = st.columns(2)
-                if x.size != 0:
-                    with ins:
-                        st.write("Input variables", x)
-                if y.size != 0:
-                    with outs:
-                        st.write("Target variable", y)
-            x = x.to_numpy()
-            y = y.to_numpy()
-
-    return x, y, input_names, output_name
+        with st.expander("See chosen data"):
+            ins, outs = st.columns(2)
+            if X_vals.size != 0:
+                with ins:
+                    st.write("Input variables", X_vals)
+            if Y_vals.size != 0:
+                with outs:
+                    st.write("Target variable", Y_vals)
+        X_vals = X_vals.to_numpy()
+        Y_vals = Y_vals.to_numpy()
+    return X_vals, Y_vals, X_names, Y_name
