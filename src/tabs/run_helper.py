8733a4135e448a2b1502e4fc296f80c20a537f7f
han.le@aalto.fi <le.nm.han@gmail.com>
2024-06-26 22:52:59 +0300

    moved some funcs from RunBOSS to RunHelper

--- src/tabs/run_helper.py
+++ src/tabs/run_helper.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import streamlit as st
 from streamlit.runtime.uploaded_file_manager import UploadedFile
@@ -6,16 +5,40 @@ from streamlit.runtime.uploaded_file_manager import UploadedFile
 
 class RunHelper:
     def __init__(self) -> None:
+        self.file = None
         self.data = None
+        self.bo_has_run = False
 
-    def download(self) -> None:
+    def update_has_run(self):
+        if self.bo_has_run:
+            self.bo_has_run = False
+
+    def upload_file(self) -> pd.DataFrame:
+        """
+        Widget to upload a file, which is read into a dataframe.
+        Display the "help" tip when hovering the mouse over the question mark icon.
+        """
+        self.file = st.file_uploader(
+            label="Restart by uploading a csv file",
+            type=["csv"],
+            help="Your file should contain data for input variables and target variable",
+        )
+        if isinstance(self.file, UploadedFile):
+            try:
+                df = pd.read_csv(self.file, sep=":|;|,")
+                return df
+            except ValueError as err:
+                st.error(
+                    "Error: "
+                    + str(err)
+                    + ". Please check the file contents and re-upload."
+                )
+
+    @staticmethod
+    def download(df) -> None:
         st.download_button(
             label="Download",
-            data=self.data.to_csv(index=False).encode("utf-8"),
+            data=df.to_csv(index=False).encode("utf-8"),
             mime="text/csv",
             key="download_acq",
         )
-
-
-
-
