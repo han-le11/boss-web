ce1e236de8cb6bf8cd38202b5d8edb514f949de4
han.le@aalto.fi <le.nm.han@gmail.com>
2024-07-17 16:11:54 +0300

    update the homepage

--- src/home.py
+++ src/home.py
@@ -2,7 +2,7 @@ import streamlit as st
 from ui.page_config import PageConfig, customize_footer
 
 page_config = PageConfig(
-    main_title="🙌 Welcome to BOSS!",
+    main_title="BOSS web app",
     tab_title="Home - Bayesian Optimization Structure Search",
     header="Home",
     icon="🏠",
@@ -11,11 +11,21 @@ page_config = PageConfig(
 page_config.set_page()
 customize_footer()
 
-st.markdown(
-    "Bayesian Optimization Structure Search (BOSS) is a general-purpose Bayesian Optimization tool. \n"
-    "It is designed to facilitate machine learning in computational and experimental natural sciences.\n"
-)
+boss_url = "https://sites.utu.fi/boss/"
 
 st.markdown(
-    "Please consult the manual page for more detailed documentation and tutorials."
+    "Bayesian Optimization Structure Search (BOSS) is a general-purpose Bayesian Optimization tool that can facilitate "
+    "machine learning in computational and experimental natural sciences.\n"
 )
+st.markdown(f"You are on the BOSS web app, which is a spin-off of the Python library [BOSS]({boss_url}).")
+st.markdown(f"Visit the [BOSS website]({boss_url}) for more information about features of the library and research use "
+            f"cases.")
+
+st.title("Contact")
+
+st.markdown("If you have any problems, questions, or suggestions for the BOSS web app, please contact us via Slack or "
+            "email: \n")
+
+st.markdown("Han Le: han.le@aalto.fi")
+st.markdown("Joakim Löfgren: joakim.lofgren@aalto.fi")
+st.markdown("Matthias Stosiek: matthias.stosiek@aalto.fi or matthias.stosiek@tum.de")
