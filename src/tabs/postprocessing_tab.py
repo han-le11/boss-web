814d49eb663ff87d3c55f5a43e64c6f367c313b8
han.le@aalto.fi <le.nm.han@gmail.com>
2025-04-04 22:24:23 +0300

    First version of input fields for fixed variables

--- src/tabs/postprocessing_tab.py
+++ src/tabs/postprocessing_tab.py
@@ -5,14 +5,39 @@ from PIL import Image
 
 
 class PostprocessingTab:
-    def __init__(self, bo_results, x_names) -> None:
+    def __init__(self, bo_results, X_names: list) -> None:
+        """
+        Initialize the PostprocessingTab class.
+
+        :param bo_results: BO results object.
+        :param X_names: List of X-axis names.
+
+        """
         self.bo_results = bo_results
-        self.x_names = x_names
-        self.expander = None
+        self.X_names: list = X_names
         self.model_plots: list[Image] = []
         self.uncert_plots: list[Image] = []
-        self.model_slice = [None, None, None]
-        self.slider_value = st.session_state.cur_iter
+        self.model_slice: list = [None, None, None]
+        self.fixed_vars: dict[str, float] = {}
+        self.slider_value: int = st.session_state.cur_iter
+
+    @property
+    def fixed_keys(self):
+        # fixed variable names are X_names excluding the variables in model slice
+        return [self.X_names[i] for i in range(len(self.X_names)) if i+1 not in self.model_slice]
+
+    @property
+    def fixed_values(self):
+        return {var: self.fixed_vars.get(var, 0.0) for var in self.fixed_keys}
+
+    # TODO: write unit test
+    def set_var_default(self):
+        """
+        Set default values for fixed variables.
+        """
+        # for each fixed variable name, make a number input field
+        for var in self.fixed_keys:
+            self.fixed_vars[var] = st.number_input(label="Fixed value for " + var, value=0.0, key="fixed_" + var)
 
     # TODO: write unit test
     def set_model_slice(self) -> None:
@@ -24,16 +49,16 @@ class PostprocessingTab:
         st.write("Which axes (max 2D) of the objective function to plot?")
         col1, col2, col3 = st.columns(3)
         with col1:
-            x1: str = st.selectbox("X1-axis", options=self.x_names)
+            x1: str = st.selectbox("X1-axis", options=self.X_names)
         with col2:
-            x2: str = st.selectbox("X2-axis", options=self.x_names, index=1)
+            x2: str = st.selectbox("X2-axis", options=self.X_names, index=1)
         with col3:
             self.model_slice[2] = st.number_input(
                 "Number of points per axis in the grid", value=50, step=1, min_value=1
             )
-        # Access index of x1 and x2 in x_names
-        self.model_slice[0] = self.x_names.index(x1) + 1
-        self.model_slice[1] = self.x_names.index(x2) + 1
+        # Access index of x1 and x2 in X_names
+        self.model_slice[0] = self.X_names.index(x1) + 1
+        self.model_slice[1] = self.X_names.index(x2) + 1
         if self.model_slice[0] == self.model_slice[1]:
             st.warning("Please select different axes to display contour plots.")
 
@@ -69,36 +94,24 @@ class PostprocessingTab:
 
     @staticmethod
     def prev_image():
+        """
+        Move to the previous image.
+        """
         if st.session_state.cur_iter > 0:
             st.session_state.cur_iter -= 1
 
     def load_plots(self) -> None:
+        """
+        Load model and uncertainty plots from the local folder.
+        """
         model_dir = "./postprocessing/graphs_models"
         if os.path.isdir(model_dir):
             self._show_plots(path=model_dir, warning=None)
 
     def update_slider_value(self):
+        """
+        Update the slider value.
+        """
         st.session_state.cur_iter = self.slider_value
         st.rerun()
 
-    # TODO: implement this to display convergence and hyperparams plots
-    def conv_hyperparams_plots(self) -> None:
-        """
-        Display plots of the convergence measures and hyperparameters.
-        """
-        col1, col2 = st.columns(2)
-        with col1:
-            st.image("./postprocessing/graphs_convergence/convergence.png", width=500)
-        with col2:
-            st.image("./postprocessing/graphs_convergence/hyperparameters.png", width=500)
-        pass
-
-    # TODO: ensure that this function works with num_iters
-    def input_pp_iters(self):
-        pp_iters = st.multiselect(
-            "Which iterations to run post-processing?",
-            help="Can't be chosen if there is no iteration.",
-            options=np.arange(0, np.arange(0, self.bo_results.num_iters)),
-            default=None,
-        )
-        return pp_iters
